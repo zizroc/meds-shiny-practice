4adfd87c3512b0e4a9c4e03a594d5b40d654e0e7
Marcus Thomson <zizroc@nceas.ucsb.edu>
2022-02-18 11:10:35 -0800

    deployed on shinyapps.io

--- myapp/app.R
+++ myapp/app.R
@@ -5,65 +5,74 @@ library(tidyverse)
 library(DT)
 
 # user interface ----
-ui <- fluidPage(# app title ----
-                tags$h1("Marcus' Palmer Penguin App"), 
-                
-                # app subtitle ----
-                p(strong("Exploring Antarctic Punguins and Temperatures")), 
-                
-                # body mass slider input ----
-                sliderInput(inputId = "body_mass", 
-                            label = "Select a range of body masses (g)", 
-                            min = 2700, max = 6300, value = c(3000, 4000)), 
-                
-                # body mass output ----
-                shiny::plotOutput(outputId = "bodyMass_scatterPlot"), 
-                
-                # body mass table output ----
-                DT::dataTableOutput(outputId = "bodyMass_dataTable")
-                
-                )
+ui <- fluidPage(
+  # app title ----
+  tags$h1("Marcus' Palmer Penguin App"),
+  
+  # app subtitle ----
+  p(strong(
+    "Exploring Antarctic Punguins and Temperatures"
+  )),
+  
+  # body mass slider input ----
+  sliderInput(
+    inputId = "body_mass",
+    label = "Select a range of body masses (g)",
+    min = 2700,
+    max = 6300,
+    value = c(3000, 4000)
+  ),
+  
+  # body mass output ----
+  shiny::plotOutput(outputId = "bodyMass_scatterPlot"),
+  
+  # body mass table output ----
+  DT::dataTableOutput(outputId = "bodyMass_dataTable")
+  
+)
 
 # break ----
 
 # server instructions ----
-server <- function(input, output){
-  
+server <- function(input, output) {
   # render scatterplot ----
   output$bodyMass_scatterPlot <- renderPlot({
-    
     # filter body masses ----
     body_mass_df <- reactive({
-      penguins %>% 
+      penguins %>%
         filter(body_mass_g %in% input$body_mass[1]:input$body_mass[2])
     })
     
     # code to generate scatterplot here
-    ggplot(na.omit(body_mass_df()), 
-           aes(x = flipper_length_mm, y = bill_length_mm, 
-               color = species, shape = species)) + 
-      geom_point() + 
-      scale_color_manual(values = c("Adelie" = "#FEA346", 
-                                    "Chinstrap" = "#B251F1", 
-                                    "Gentoo" = "#4BA4A4")) +
-      scale_shape_manual(values = c(
-        "Adelie" = 19, 
-        "Chinstrap" = 17, 
-        "Gentoo" = 15
-      )) + 
-      labs(
-        x = "Flipper length (mm)", 
-        y = "Bill length (mm)"
+    ggplot(
+      na.omit(body_mass_df()),
+      aes(
+        x = flipper_length_mm,
+        y = bill_length_mm,
+        color = species,
+        shape = species
       )
+    ) +
+      geom_point() +
+      scale_color_manual(values = c(
+        "Adelie" = "#FEA346",
+        "Chinstrap" = "#B251F1",
+        "Gentoo" = "#4BA4A4"
+      )) +
+      scale_shape_manual(values = c(
+        "Adelie" = 19,
+        "Chinstrap" = 17,
+        "Gentoo" = 15
+      )) +
+      labs(x = "Flipper length (mm)",
+           y = "Bill length (mm)")
   })
   
-  output$bodyMass_dataTable <- DT::renderDataTable(
-    DT::datatable(penguins, 
-                  caption = "hello", 
-                  options = list(
-                    pageLength = 5
-                  ))
-  )
+  output$bodyMass_dataTable <- DT::renderDataTable(DT::datatable(
+    penguins,
+    caption = "hello",
+    options = list(pageLength = 5)
+  ))
   
 }
 
