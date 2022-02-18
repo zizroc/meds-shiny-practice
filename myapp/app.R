3e4208cabe2af9094e86d6d989535d52b7ae68aa
Marcus Thomson <zizroc@nceas.ucsb.edu>
2022-02-18 10:30:14 -0800

    Now the legend colours and shapes stay consistent regardless of slider position

--- myapp/app.R
+++ myapp/app.R
@@ -38,7 +38,14 @@ server <- function(input, output){
            aes(x = flipper_length_mm, y = bill_length_mm, 
                color = species, shape = species)) + 
       geom_point() + 
-      scale_color_manual(values = c("red", "purple", "green")) + 
+      scale_color_manual(values = c("Adelie" = "#FEA346", 
+                                    "Chinstrap" = "#B251F1", 
+                                    "Gentoo" = "#4BA4A4")) +
+      scale_shape_manual(values = c(
+        "Adelie" = 19, 
+        "Chinstrap" = 17, 
+        "Gentoo" = 15
+      )) + 
       labs(
         x = "Flipper length (mm)", 
         y = "Bill length (mm)"
