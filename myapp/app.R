d1c58f2f689974ca0e8c69cd15340953712e7657
Marcus Thomson <zizroc@nceas.ucsb.edu>
2022-02-18 09:45:57 -0800

    added text

---
+++ myapp/app.R
@@ -0,0 +1,7 @@
+library(shiny)
+
+ui <- fluidPage(# app title ----
+                tags$h1("Marcus' app"), 
+                
+                # app subtitle ----
+                p(strong("This is great")))
